8e74058ae6d755c17b3d13440abed7f6f3b43007
Luciano Ferreira <luciano2023silva@gmail.com>
2021-07-06 18:40:48 -0300

    fixing bug in webhooks.ts

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,55 +1,50 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
-import { Readable } from 'stream';
 import { stripe } from '../../services/stripe';
+import { Readable } from 'stream';
 import Stripe from 'stripe';
-import { resolveTxt } from 'dns';
 
 async function buffer(readable: Readable) {
     const chunks = [];
 
     for await (const chunk of readable) {
         chunks.push(
-            typeof chunk === 'string' ? Buffer.from(chunk) : chunk
-        )
+            typeof chunk === "string" ? Buffer.from(chunk) : chunk
+        );
     }
 
-    return Buffer.concat(chunks)
+    return Buffer.concat(chunks);
 }
+
 export const config = {
     api: {
-        bodyParse: false
+        bodyParser: false
     }
 }
 
-const relevantEvents = new Set([
+const  relevantEvents = new Set([
     'checkout.session.completed'
 ])
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function (req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        const buf = await buffer(req);
+        const buf = await buffer(req)
         const secret = req.headers['stripe-signature']
 
         let event: Stripe.Event;
+
         try {
-            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
-            console.log('chegou no try:',event)
+            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
 
         } catch (err) {
-            console.log('Errooo!! ' + err)
-
-            return res.status(400).send(`Webhook error ${err.message}`)
+            return res.status(400).send(`Webhook error: ${err.message}`)
         }
-
         const { type } = event;
-        console.log(type)
-        
+
         if (relevantEvents.has(type)) {
-            console.log('Evento recebido', event)
+            console.log(`Evento recebido ${event}`)
         }
 
-        res.json({ received: true })
+        res.status(200).json({ ok: true })
     } else {
         res.setHeader('Allow', 'POST')
         res.status(405).end('Method not allowed')
