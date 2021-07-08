6b3437c006f9726c9c4e667938a382b32bad05e9
Luciano Ferreira <luciano2023silva@gmail.com>
2021-07-08 12:53:47 -0300

    litening especifics event from stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,7 +23,9 @@ export const config = {
 }
 
 const  relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscriptions.updated',
+    'customer.subscriptions.deleted'
 ])
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
@@ -44,12 +46,22 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         if (relevantEvents.has(type)) {
             try {
                 switch (type) {
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+                        const subscription = event.data.object as Stripe.Subscription;
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false
+                        )
+                        console.log(subscription.customer.toString())
+                        break;
                     case 'checkout.session.completed':
-
                         const checkoutSession = event.data.object as Stripe.Checkout.Session
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
+                            checkoutSession.customer.toString(),
+                            true
                         )
 
                         break;
