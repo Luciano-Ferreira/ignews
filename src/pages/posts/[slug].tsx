ca74022b42c596cf45fc2d75b4ab8dc0f21ea294
Luciano Ferreira <luciano2023silva@gmail.com>
2021-08-17 17:15:46 -0300

    page: preview post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -45,7 +45,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
 
   console.log(session)
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
        redirect: {
          destination: '/',
