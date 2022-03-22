9fff02e3b729ce4ac41e78a74d13a273037be34a
Luciano Ferreira <luciano2023silva@gmail.com>
2022-03-22 19:32:59 -0300

    testing Header

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ActiveLink } from '.';
 
 jest.mock('next/router', () => {
@@ -13,7 +13,7 @@ jest.mock('next/router', () => {
 
 describe('ActiveLink component', () => {
   it('should be able active link renders correctly', () => {
-    const { getByText } = render(
+    render(
       <ActiveLink
         href='/'
         activeClassName='active'
@@ -22,11 +22,11 @@ describe('ActiveLink component', () => {
       </ActiveLink>
     )
   
-    expect(getByText('Home')).toBeInTheDocument()
+    expect(screen.getByText('Home')).toBeInTheDocument()
   });
   
   it('should be able active link is receiving active class', () => {
-    const { getByText } = render(
+    render(
       <ActiveLink
         href='/'
         activeClassName='active'
@@ -35,7 +35,7 @@ describe('ActiveLink component', () => {
       </ActiveLink>
     )
   
-    expect(getByText('Home')).toHaveClass('active')
+    expect(screen.getByText('Home')).toHaveClass('active')
   });
 })
 
