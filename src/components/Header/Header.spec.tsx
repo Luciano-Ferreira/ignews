87c79dd575a77c6132167c8e4a9a1103c5d7e9aa
Luciano Ferreira <luciano2023silva@gmail.com>
2022-03-22 21:34:59 -0300

    SignInButton test

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -11,7 +11,7 @@ jest.mock('next/router', () => {
   }
 });
 
-jest.mock('next-auth/client', () => {
+jest.mock('next-auth/react', () => {
   return {
     useSession() {
       return [null, false]
