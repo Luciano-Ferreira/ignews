d3df93ddc8156915b77f348045ba3994982b6e13
Luciano Ferreira <luciano2023silva@gmail.com>
2022-03-26 00:30:44 -0300

    test Posts page

--- src/tests/pages/Posts.spec.tsx
+++ src/tests/pages/Posts.spec.tsx
@@ -25,5 +25,44 @@ describe('Posts page', () => {
     expect(screen.getByText('My new post')).toBeInTheDocument()
   });
 
+  it('loads initial data', async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient);
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      query: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: 'my-new-post',
+            data: {
+              title: [
+                { type: 'heading', text: 'My new post' }
+              ],
+              content: [
+                { type: 'paragraph', text: 'Post excerpt' }
+              ]
+            },
+            last_publication_date: '04-01-2021'
+          }
+        ]
+      })
+    } as any)
+
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        prop: {
+          posts: [{
+            slug: 'my-new-post',
+            title: 'My new post',
+            excerpt: 'Post excerpt',
+            updatedAt: '01 de Abril de 2021'
+          }]
+        }
+      })
+    )
+  })
+
   
 });
