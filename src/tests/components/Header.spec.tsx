f675cc27cc3d0870dc5561ce8417869e7e0cf2f2
Luciano Ferreira <luciano2023silva@gmail.com>
2022-04-03 17:52:36 -0300

    add coverage rules

--- src/tests/components/Header.spec.tsx
+++ src/tests/components/Header.spec.tsx
@@ -24,8 +24,6 @@ describe('Header component', () => {
     render(
       <Header />
     )
-    screen.logTestingPlaygroundURL()
-
 
     expect(screen.getByText('Home')).toBeInTheDocument()
     expect(screen.getByText('Posts')).toBeInTheDocument()
